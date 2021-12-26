72b1049f1e803e363795c773301ac2518b545f4e
harguilar <30771959+harguilar@users.noreply.github.com>
2021-12-26 21:44:25 +0100

    added cached Network Image Widget

---
+++ lib/ui/widgets/cach_network_image/cached_network_image_wrapper.dart
@@ -0,0 +1,24 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CachedNetworkImageWrapper extends StatelessWidget {
+  final String imageUrl;
+  final Widget? loader;
+  final Widget? errorWidget;
+
+  const CachedNetworkImageWrapper(
+      {Key? key, required this.imageUrl, this.loader, this.errorWidget})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      placeholder: (BuildContext context, String url) =>
+          loader ?? CircularProgressIndicator(),
+      errorWidget: (BuildContext context, String url, dynamic error) =>
+          errorWidget ?? Image.asset('assets/icons/sem-internet.jpg'),
+      fit: BoxFit.fill,
+    );
+  }
+}
