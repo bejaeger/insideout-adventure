0394ea4556b2928195cc84128b7d36a1420c5d8f
Benjamin Paul Jaeger <benjamin.jaeger@cern.ch>
2022-12-04 10:05:34 -0800

    remove iOS background fetch

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,28 +1,15 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import Firebase
-import FirebaseCore
-import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      GMSServices.provideAPIKey("AIzaSyCymdYYa-jJCHbAsfewAvOULRL1yTkSoSI")
-      FirebaseApp.configure()
-      application.registerForRemoteNotifications()
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    GMSServices.provideAPIKey("AIzaSyCymdYYa-jJCHbAsfewAvOULRL1yTkSoSI")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-    override func application(_ application: UIApplication,
-                              didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-        print("Token: \(deviceToken)")
-        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    }
 }
