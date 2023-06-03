e018bc95b0d18ea369039225198040e930818498
Areeb Shams <ardevsha@gmail.com>
2023-06-03 13:16:52 -0400

    Update Firestore migration script
    - The 'c|Child -> w|Ward' change was too aggressive
      - Fixed it by excluding double-quotes which worked for the dataset
        that was in the export

--- tools/firestore_migrations/2023-05-20.1.sh
+++ tools/firestore_migrations/2023-05-20.1.sh
@@ -6,7 +6,7 @@ sed -i '' 's/parent/guardian/g' *.json # Should be a 'No Operation'. Included fo
 sed -i '' 's/Parent/Guardian/g' *.json # Should be a 'No Operation'. Included for completeness.
 sed -i '' 's/children/wards/g' *.json # Should be a 'No Operation'. Included for completeness.
 sed -i '' 's/Children/Wards/g' *.json # Should be a 'No Operation'. Included for completeness.
-sed -i '' 's/child/ward/g' *.json # Should be a 'No Operation'. Included for completeness.
-sed -i '' 's/Child/Ward/g' *.json # Should be a 'No Operation'. Included for completeness.
+sed -i '' -r 's/([^"])child)/\1ward/g' *.json # Should be a 'No Operation'. Included for completeness.
+sed -i '' -r 's/([^"])Child/\1Ward/g' *.json # Should be a 'No Operation'. Included for completeness.
 sed -i '' 's/explorer/ward/g' *.json
 sed -i '' 's/Explorer/Ward/g' *.json
